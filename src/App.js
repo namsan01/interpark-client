af573107fbbb483878bb4ed570f029221a59bef2
namsan01 <namsan2934@gmail.com>
2023-11-06 18:09:23 +0900

    feat live.css 섹션 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Recommand from "./components/Recommend";
+import Visual from "./components/Visual";
 
 function App() {
+  // 안녕하세요. js 자리입니다.
+  /* 여러 줄 설명이에요 */
   return (
     <div className="wrap">
+      {/* <!-- 상단 영역 --> */}
       <Header />
-      <div className="main">메인</div>
-      <Footer />
+      {/* <!-- 메인 영역 --> */}
+      <div className="main">
+        <Visual />
+        <Recommand />
+      </div>
+      {/* <!-- 하단 영역 --> */}
+      <footer className="footer"></footer>
     </div>
   );
 }
